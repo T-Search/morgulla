9db656b97d3f21095179fd032737d24f0d78b870
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-06 16:03:16 +0200

    Add basic footer with version and git hash

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,25 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
+
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { ChildProcess } from 'child_process';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
+
+let rev = readFileSync('.git/HEAD').toString().trim();
+if (rev.indexOf(':') === -1) {
+} else {
+	rev = readFileSync('.git/' + rev.substring(5))
+		.toString()
+		.trim();
+}
+
+rev = rev.substring(0, 7);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,7 +27,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({ out: 'build' })
+		adapter: adapter({ out: 'build' }),
+		vite: {
+			define: {
+				__APP_VERSION__: JSON.stringify(pkg.version),
+				__COMMIT_HASH__: JSON.stringify(rev)
+			}
+		}
 	}
 };
 
