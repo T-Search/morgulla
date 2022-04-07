64474dd5876488090628a79b3c4197f112a670ec
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-08 01:19:05 +0200

    Add under construction notice

---
+++ src/routes/highlights.svelte
@@ -0,0 +1,3 @@
+<div class="p-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800" role="alert">
+  <span class="font-medium">Not available!</span> This search is still under development and not yet available.
+</div>
