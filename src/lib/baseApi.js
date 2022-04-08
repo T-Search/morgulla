474a6f255fba9675a002a6f3a57c53e22e865876
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-09 01:25:15 +0200

    Add mode based api base url

---
+++ src/lib/baseApi.js
@@ -0,0 +1,1 @@
+export const baseApi = import.meta.env.PROD ? '/api/datava' : 'http://localhost:8080';
--- src/routes/clips.svelte
+++ src/routes/clips.svelte
@@ -6,24 +6,21 @@
 	import Clip from '$lib/clip.svelte';
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	import { browser } from "$app/env";
-
-	console.log()
+	import { browser } from '$app/env';
+	import { baseApi } from '$lib/baseApi';
 
 	let q = '';
-	let broadcaster = "";
+	let broadcaster = '';
 	let clips = [];
 	let foundBroadcaster = false;
 
 	let possibleBroadcaster = [];
 
-	const __API_URL__ = "https://alpha.tsearch.tools/api/datava/";
-
 	onMount(async () => {
 		q = localStorage.getItem('q') || '';
 		broadcaster = localStorage.getItem('broadcaster') || '';
 		//Load broadcaster
-		const res = await fetch(__API_URL__ + '/broadcaster/all');
+		const res = await fetch(baseApi + '/broadcaster/all');
 		const data = await res.json();
 		possibleBroadcaster = data.map((name) => name.toLowerCase());
 		checkBroadcaster();
@@ -32,11 +29,11 @@
 
 	async function loadData() {
 		if (foundBroadcaster) {
-			if(browser) {
+			if (browser) {
 				localStorage.setItem('q', q);
 				localStorage.setItem('broadcaster', broadcaster);
 			}
-			var url = new URL(__API_URL__ + '/clip/search');
+			var url = new URL(baseApi + '/clip/search');
 			var params = { broadcaster: broadcaster, q: q, pageNumber: 0, pageSize: 20 };
 			url.search = new URLSearchParams(params).toString();
 
@@ -80,14 +77,14 @@
 			? 'border-green-500 dark:border-green-400 focus:ring-green-500 text-green-500'
 			: 'border-red-500 dark:border-red-400 focus:ring-red-500 text-red-500'}"
 		placeholder="Broadcaster"
-		value="{broadcaster}"
+		value={broadcaster}
 		on:input={handleBroadcasterInput}
 	/>
 	<input
 		type="search"
 		class="w-9/12 max-w-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 		placeholder="Bonnie Green"
-		value="{q}"
+		value={q}
 		on:input={handleSearchInput}
 	/>
 	<button
@@ -95,11 +92,10 @@
 		class="inline-flex items-center px-3 border-l-0 text-blue-700 hover:text-white border dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600 border-gray-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm  text-center dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
 		><Fa icon={faMagnifyingGlass} /></button
 	>
-		<button
+	<button
 		type="button"
 		class="inline-flex items-center px-3 rounded-r-md border-l-0 text-blue-700 hover:text-white border dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600 border-gray-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm  text-center dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
-		on:click="{loadData}"
-		><Fa icon={faRotate} /></button
+		on:click={loadData}><Fa icon={faRotate} /></button
 	>
 </div>
 <div class="flex flex-col flex-wrap content-center gap-2 pt-3 pb-3">
