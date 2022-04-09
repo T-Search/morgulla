924f67b6c2df4ba8665339f6cac027241b9f9470
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-10 01:38:11 +0200

    Add broadcaster name autocomplete

--- src/routes/clips.svelte
+++ src/routes/clips.svelte
@@ -103,7 +103,13 @@
 		placeholder="Broadcaster"
 		value={broadcaster}
 		on:input={handleBroadcasterInput}
+		list="broadcaster-autocomplete"
 	/>
+	<datalist id="broadcaster-autocomplete">
+		{#each possibleBroadcaster as broadcaster}
+			<option>{broadcaster}</option>
+		{/each}
+	</datalist>
 	<input
 		type="search"
 		class="w-9/12 max-w-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
