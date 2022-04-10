4fc1093d64b02892d8f75d255448fbc11594ee3c
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-10 17:38:57 +0200

    Highlight selected page in navbar

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { page } from '$app/stores';
+
+  $: activePageName = $page.routeId;
 </script>
 
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
@@ -46,14 +48,16 @@
 				<li>
 					<a
 						href="clips"
-						class="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-900 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						class="block py-2 pr-4 pl-3 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent
+            {activePageName == "clips" ? 'text-gray-200 dark:text-gray-600' : 'text-gray-700 dark:text-gray-400'}"
 						>Clips</a
 					>
 				</li>
 				<li>
 					<a
 						href="highlights"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700
+            {activePageName == "highlights" ? 'text-gray-200 dark:text-gray-600' : 'text-gray-700 dark:text-gray-400'}"
 						>Highlights</a
 					>
 				</li>
