54941534b674009cafa71cb6d728f3c208305bda
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-13 01:07:11 +0200

    Add highlights broadcaster route

---
+++ src/routes/highlights/[broadcaster].svelte
@@ -0,0 +1,10 @@
+<script>
+	import { browser } from '$app/env';
+	import { goto } from '$app/navigation';
+    import { getStores, navigating, page, session, updated } from '$app/stores';
+
+	if (browser) {
+        localStorage.setItem('broadcaster', $page.params.broadcaster);
+		goto('/highlights');
+	}
+</script>
