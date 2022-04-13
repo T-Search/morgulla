dd0093c3b8c1966e2833c9210ac8a4d31700507c
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-14 01:44:58 +0200

    Show account name after success

--- src/routes/login/success.svelte
+++ src/routes/login/success.svelte
@@ -1,3 +1,7 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
 <svelte:head>
 	<title>Erfolgreiche Anmeldung</title>
 </svelte:head>
@@ -5,7 +9,7 @@
 <div class="flex flex-col items-center">
     <div class="p-4 max-w-3xl text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
       <span class="font-medium">Anmeldung erfolgreich!</span><br />
-      Dein Account wird nun überwacht. Es dauert ein paar Minuten bis du in der Suche als gültig markiert wirst. <br />
+      Dein Account {$page.url.searchParams.get('login')} wird nun überwacht. Es dauert ein paar Minuten bis du in der Suche als gültig markiert wirst. <br />
       Highlights sollten innerhalb von 24 Stunden geladen sein. Neue Clips sollten ebenfalls innerhalb von ein paar Stunden auftauchen.
       Bis alle Clips verfügbar sind, kann es ein paar Tage dauern.
     </div>
