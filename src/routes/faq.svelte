6ef3e968aa9db05aaaa9f4cdb4450134254e1c87
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-14 01:50:43 +0200

    Add disconnect faq

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -26,4 +26,17 @@
             Demnächst soll es für Streamer möglich sein sich mithilfe ihres Twitch Logins selbst auf der Webseite einzutragen.
         </p>
     </div>
+    <div
+        class="block p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+    >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Ich möchte meinen Stream austragen.
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+            Solltest du dich selbst angemeldet haben, kannst du dich jederzeit auch selbständig wieder austragen. 
+            Dafür musst du die Verbindung zur tsearch Anwendung bei Twitch trennen. 
+            Das kannst du in den <a href="https://www.twitch.tv/settings/connections" class="text-blue-700 hover:underline dark:text-blue-500">Twitch Einstellungen</a> machen. <br />
+            Solltest du zu den Streamern zählen die bereits ohne selbständige Eintragung sichtbar waren, kontaktiere mich bitte.
+        </p>
+    </div>
 </div>
