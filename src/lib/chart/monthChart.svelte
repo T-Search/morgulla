3c56895e77c26a4883a44ebc9f4a081eee65c351
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-06-23 02:39:57 +0200

    Add first charset

---
+++ src/lib/chart/monthChart.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto/auto.js';
+
+	let portfolio;
+	export let data;
+	const config = {
+		type: 'bar',
+		data: data,
+		options: {
+			borderRadius: '10',
+			plugins: {
+				title: {
+					display: true,
+					text: 'Clips pro Monat'
+				}
+			},
+			responsive: true,
+			scales: {
+				y: {
+					min: 0,
+					title: {
+						display: true,
+						text: 'Erstellte Clips'
+					}
+				},
+				y1: {
+					display: true,
+					position: 'right',
+
+					min: 0,
+					title: {
+						display: true,
+						text: 'Erstellte Clips in %'
+					}
+				}
+			}
+		}
+	};
+	onMount(() => {
+		const ctx = portfolio.getContext('2d');
+		// Initialize chart using default config set
+		var monthChart = new Chart(ctx, config);
+	});
+</script>
+
+<canvas bind:this={portfolio} />
---
+++ src/routes/stats.svelte
@@ -0,0 +1,14 @@
+<script>
+	import MonthChart from '$lib/chart/monthChart.svelte';
+
+
+    const t = {"labels":["Dezember 2021","Januar 2022","Februar 2022","März 2022","April 2022","Mai 2022"],"datasets":[{"label":"Creator","data":[86,205,352,296,373,515],"type":"bar","hidden":false,"backgroundColor":"#f28e2b","borderColor":"#f28e2b","yAxisID":"y"},{"label":"Creator %","data":[4.71,11.22,19.27,16.2,20.42,28.19],"type":"line","hidden":false,"backgroundColor":"#7f4f1e","borderColor":"#7f4f1e","yAxisID":"y1"},{"label":"Andere Streamer %","data":[13.78,15.82,14.54,15.32,20.24,20.29],"type":"line","hidden":false,"backgroundColor":"#e15759","borderColor":"#e15759","yAxisID":"y1"}]}
+</script>
+
+<svelte:head>
+	<title>Statistiken</title>
+</svelte:head>
+
+<div class="w-1/3">
+	<MonthChart data={t} />
+</div>
