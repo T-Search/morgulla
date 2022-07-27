421f0a85430dbb310e44622ae6d0d23956f992e4
Skillkiller <seblsebastian@aol.de>
2022-07-27 02:52:48 +0200

    Add box stats

---
+++ src/lib/chart/box.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let title: string;
+	export let count: number;
+	export let last30days: number | null = null;
+	export let color: 'red' | 'blue' | 'green';
+</script>
+
+<div class="rounded-lg custom-bg-{color} text-center text-white">
+	<span class="font-bold text-2xl">{count.toLocaleString()}</span>
+    {#if last30days != null}
+	<span class="inline-flex"
+		>+ {last30days.toLocaleString()}
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="26"
+			height="26"
+			fill="currentColor"
+			class="h-5 mr-2 bi bi-arrow-up-right-square-fill"
+			viewBox="0 0 16 16"
+		>
+			<path
+				d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z"
+			/>
+		</svg>
+	</span>
+    {/if}
+
+	<p class="text-xl">{title}</p>
+</div>
+
+<style>
+	.custom-bg-blue {
+		background-color: #3c69e7;
+	}
+
+	.custom-bg-red {
+		background-color: #e52b50;
+	}
+
+	.custom-bg-green {
+		background-color: #3b7a57;
+	}
+</style>
--- src/routes/stats.svelte
+++ src/routes/stats.svelte
@@ -1,14 +1,15 @@
 <script>
-	import MonthChart from '$lib/chart/monthChart.svelte';
+	import Box from '$lib/chart/box.svelte';
 
 
-    const t = {"labels":["Dezember 2021","Januar 2022","Februar 2022","März 2022","April 2022","Mai 2022"],"datasets":[{"label":"Creator","data":[86,205,352,296,373,515],"type":"bar","hidden":false,"backgroundColor":"#f28e2b","borderColor":"#f28e2b","yAxisID":"y"},{"label":"Creator %","data":[4.71,11.22,19.27,16.2,20.42,28.19],"type":"line","hidden":false,"backgroundColor":"#7f4f1e","borderColor":"#7f4f1e","yAxisID":"y1"},{"label":"Andere Streamer %","data":[13.78,15.82,14.54,15.32,20.24,20.29],"type":"line","hidden":false,"backgroundColor":"#e15759","borderColor":"#e15759","yAxisID":"y1"}]}
 </script>
 
 <svelte:head>
 	<title>Statistiken</title>
 </svelte:head>
 
-<div class="w-1/3">
-	<MonthChart data={t} />
+<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+  <Box title="Highlights" count={4168} last30days={184} color="blue"></Box>
+  <Box title="Clips" count={17320} last30days={259} color="red"></Box>
+  <Box title="Gespielte Spiele" count={83} color="green"></Box>
 </div>
