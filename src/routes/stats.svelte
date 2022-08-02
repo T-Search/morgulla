63f93123019ed16039ce33bee0c078f50a5de4df
Skillkiller <seblsebastian@aol.de>
2022-08-03 01:08:36 +0200

    Add top clipper statistics

--- src/routes/stats.svelte
+++ src/routes/stats.svelte
@@ -16,111 +16,7 @@
 		day: '2-digit'
 	};
 
-		const d = {
-		boxStatistics: {
-			highlights: 4201,
-			highlightsLast30Days: 190,
-			clips: 17277,
-			clipsLast30Days: 253,
-			games: 84
-		},
-		highlightsPerMonth: {
-			labels: [
-				'Juli 2021',
-				'August 2021',
-				'September 2021',
-				'Oktober 2021',
-				'November 2021',
-				'Dezember 2021',
-				'Januar 2022',
-				'Februar 2022',
-				'März 2022',
-				'April 2022',
-				'Mai 2022',
-				'Juni 2022'
-			],
-			datasets: [
-				{ name: 'Highlights', data: [68, 70, 110, 109, 140, 195, 248, 245, 250, 318, 292, 207] }
-			]
-		},
-		highlightsPerWeekday: {
-			labels: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
-			datasets: [{ name: 'Clips', data: [2217, 1277, 2653, 2821, 2599, 2405, 3305] }]
-		},
-		clipsPerMonth: {
-			labels: [
-				'Juli 2021',
-				'August 2021',
-				'September 2021',
-				'Oktober 2021',
-				'November 2021',
-				'Dezember 2021',
-				'Januar 2022',
-				'Februar 2022',
-				'März 2022',
-				'April 2022',
-				'Mai 2022',
-				'Juni 2022'
-			],
-			datasets: [
-				{ name: 'Clips', data: [361, 250, 198, 173, 140, 86, 189, 341, 290, 371, 557, 349] }
-			]
-		},
-		clipsPerHour: {
-			labels: [
-				'0',
-				'1',
-				'2',
-				'3',
-				'4',
-				'5',
-				'6',
-				'7',
-				'8',
-				'9',
-				'10',
-				'11',
-				'12',
-				'13',
-				'14',
-				'15',
-				'16',
-				'17',
-				'18',
-				'19',
-				'20',
-				'21',
-				'22',
-				'23'
-			],
-			datasets: [
-				{
-					name: 'Clips',
-					data: [
-						1878, 1822, 1559, 1104, 751, 495, 889, 951, 741, 529, 305, 246, 171, 200, 190, 162, 136,
-						186, 171, 210, 283, 829, 1569, 1900
-					]
-				}
-			]
-		},
-		clipsPerGame: {
-			labels: [
-				'Dead by Daylight',
-				'Hunt: Showdown',
-				'Deathgarden: BLOODHARVEST',
-				'Far Cry 5',
-				'Unbekannt',
-				'Apex Legends',
-				'Phasmophobia',
-				'Sekiro: Shadows Die Twice',
-				'A Plague Tale: Innocence',
-				'Vampire: The Masquerade - Bloodhunt',
-				'75 andere Spiele'
-			],
-			datasets: [{ name: 'Spiele', data: [14737, 1349, 233, 76, 53, 47, 35, 33, 31, 26, 657] }]
-		},
-		calculatedAt: '2022-07-30T23:50:08.963115065Z'
-	};
+		const d = {"boxStatistics":{"highlights":4213,"highlightsLast30Days":167,"clips":17309,"clipsLast30Days":225,"games":84},"highlightsPerMonth":{"labels":["August 2021","September 2021","Oktober 2021","November 2021","Dezember 2021","Januar 2022","Februar 2022","März 2022","April 2022","Mai 2022","Juni 2022","Juli 2022"],"datasets":[{"name":"Highlights","data":[70,110,109,140,195,248,245,250,318,292,207,202]}]},"highlightsPerWeekday":{"labels":["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"],"datasets":[{"name":"Highlights","data":[843,206,571,714,624,569,686]}]},"clipsPerMonth":{"labels":["August 2021","September 2021","Oktober 2021","November 2021","Dezember 2021","Januar 2022","Februar 2022","März 2022","April 2022","Mai 2022","Juni 2022","Juli 2022"],"datasets":[{"name":"Clips","data":[250,198,173,140,86,189,341,290,371,557,349,283]}]},"clipsPerHour":{"labels":["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"],"datasets":[{"name":"Clips","data":[1881,1823,1563,1112,752,498,890,951,742,529,305,246,171,200,190,162,136,185,172,209,282,831,1576,1903]}]},"clipsPerGame":{"labels":["Dead by Daylight","Hunt: Showdown","Deathgarden: BLOODHARVEST","Far Cry 5","Unbekannt","Apex Legends","Phasmophobia","Sekiro: Shadows Die Twice","A Plague Tale: Innocence","Vampire: The Masquerade - Bloodhunt","75 andere Spiele"],"datasets":[{"name":"Spiele","data":[14768,1353,229,76,53,47,35,33,31,26,658]}]},"clipperPerCount":{"labels":["Gammalunatic","Sonnen_blume95","chainsawface","Icewoman","CrimsonFrost87","VanilleColax","1SaltySurvivor","Kahron","Leonie__________","Unbekannt","3834 andere Clipper"],"datasets":[{"name":"Clipper","data":[3826,981,578,328,233,191,173,159,152,149,10539]}]},"clipperPerViews":{"labels":["Gammalunatic","Icewoman","Simplyplayer","Lanzurada","CatNight","Unbekannt","Sonnen_blume95","Kahron","CrimsonFrost87","D4sW1ck","3834 andere Clipper"],"datasets":[{"name":"Clipper","data":[21268,15984,9395,4968,4258,3517,3210,3055,2968,2637,85960]}]},"calculatedAt":"2022-08-02T23:01:58.979977331Z"};
 </script>
 
 <svelte:head>
@@ -186,6 +82,18 @@
 	</div>
 	<div class="col-span-1">
 		<PiChart chartTitle="Erstelle Clips nach Spiel" labels={d.clipsPerGame.labels} dataset={d.clipsPerGame.datasets[0]} />
+	</div>
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 pt-5">
+	<div class="col-span-1 md:col-span-2 text-4xl text-center bg-[#3c69e7] p-2 rounded-lg text-white">
+		<span>Clipper</span>
+	</div>
+	<div class="col-span-1">
+		<PiChart chartTitle="Top Clipper durch Anzahl an Clips" labels={d.clipperPerCount.labels} dataset={d.clipperPerCount.datasets[0]} />
+	</div>
+	<div class="col-span-1">
+		<PiChart chartTitle="Top Clipper durch Views an Clips" labels={d.clipperPerViews.labels} dataset={d.clipperPerViews.datasets[0]} />
 	</div>
 </div>
 
