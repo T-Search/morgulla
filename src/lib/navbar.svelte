05359219b50ec4bb610524c40aead0c1cbe1ba46
Skillkiller <seblsebastian@aol.de>
2022-08-06 03:01:57 +0200

    Add Statistiken navbar button

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -74,6 +74,15 @@
 							: 'text-gray-700 dark:text-gray-400'}">FAQ</a
 					>
 				</li>
+								<li>
+					<a
+						href="/stats"
+						class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700
+            {activePageName == 'stats'
+							? 'text-gray-200 dark:text-gray-600'
+							: 'text-gray-700 dark:text-gray-400'}">Statistiken</a
+					>
+				</li>
 				<li>
 					<a
 						href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id={import.meta
